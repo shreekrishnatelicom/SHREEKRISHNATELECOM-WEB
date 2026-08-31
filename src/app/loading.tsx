6cdda4568e9f6eca3a81b90b0730f952b4c7e1bf
shreekrishnatelicom <shreekrishnatelicomraipur@gmail.com>
2026-08-31 20:20:27 +0530

    Delete src directory

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function Loading() {
-  // Return null because the global RouteLoader component covers the page transitions
-  // with a unified full-screen loading animation, avoiding double-loader layouts.
-  return null;
-}
--- src/app/template.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="animate-page-enter min-h-screen w-full">
-      {children}
-    </div>
-  );
-}
